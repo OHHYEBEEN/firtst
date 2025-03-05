7de24544080aa8669689e17f5c140fe69901a204
OHHYEBEEN <ohb0312@naver.com>
2025-03-05 15:44:15 +0900

    dd

--- firsdjango/first/views.py
+++ firsdjango/first/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 # 화면에 글씨를 출력하는 코드
 def home(request):
-    context= {"name": "오혜빈", "food": "스테이크", "name3"}
+    context= {"name": "오혜빈", "food": "스테이크"}
     # render(html파일을 연결시켜주는 라이브러리)
     return render(request, "first/home.html", context)
